f1144c2c422127d4c631ddd9700da5bc98a6d4c6
Sma1lboy <541898146chen@gmail.com>
2025-05-22 16:22:34 -0500

    feat: add remote image patterns for Picsum and UI Avatars in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "ui-avatars.com",
+        port: "",
+        pathname: "/api/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
